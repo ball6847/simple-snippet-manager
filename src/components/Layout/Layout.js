60ac266bd36363fc08fe5e35a50471d2fe6006e9
Nattasak <dottedes@gmail.com>
2018-01-21 12:23:32 +0700

    style: header, sidebar and main section with bulma

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
+import 'bulma/css/bulma.css';
+
 import './Layout.css';
 
-const Layout = (props) => (
+const Layout = ({ children }) => (
   <div className="Layout">
-    {props.children}
+    <section className="hero">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Snippet Manager</h1>
+          <h2 className="subtitle">Useful snippet for everyone</h2>
+        </div>
+        <br/>
+        <div className="container">
+          <a className="button is-link">Add</a>
+          {' '}
+          <a className="button is-warning">Edit</a>
+        </div>
+      </div>
+    </section>
+    <div className="container">
+      <div className="columns">{children}</div>
+    </div>
   </div>
 );
 
