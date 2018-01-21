e4fbdfde21f9ccf9fd9ff5e6dd39590dba82aad1
Nattasak <dottedes@gmail.com>
2018-01-21 22:28:33 +0700

    refactor: move Header out of Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 import 'bulma/css/bulma.css';
 
@@ -6,20 +7,7 @@ import './Layout.css';
 
 const Layout = ({ children }) => (
   <div className="Layout">
-    <section className="hero">
-      <div className="hero-body">
-        <div className="container">
-          <h1 className="title">Snippet Manager</h1>
-          <h2 className="subtitle">Useful snippet for everyone</h2>
-        </div>
-        <br/>
-        <div className="container">
-          <a className="button is-link">Add</a>
-          {' '}
-          <a className="button is-warning">Edit</a>
-        </div>
-      </div>
-    </section>
+    <Header />
     <div className="container">
       <div className="columns">{children}</div>
     </div>
